5a61e60eeaf871dc1f0c42af688d9eeed8b9059d
Joe Bentley <joebentley10@gmail.com>
2016-03-28 19:53:49 +0100

    Reformatted code

--- client.js
+++ client.js
@@ -4,74 +4,74 @@ var io = require('socket.io-client');
 
 $(function() {
     attachFastClick(document.body);
-    
+
     var socket = io();
 
     // Logging function.
-    var log = function (text) {
-	$('#log').text(text + '\n' + $('#log').text());
+    var log = function(text) {
+        $('#log').text(text + '\n' + $('#log').text());
     };
 
     var touching = false;
 
-    var getPosition = function (touch) {
-	var pos = { x: touch.clientX, y: touch.clientY };
+    var getPosition = function(touch) {
+        var pos = { x: touch.clientX, y: touch.clientY };
 
-	// Normalise to canvas size.
-	pos.x = pos.x / $('#trackpad').width();
-	pos.y = pos.y / $('#trackpad').height();
+        // Normalise to canvas size.
+        pos.x = pos.x / $('#trackpad').width();
+        pos.y = pos.y / $('#trackpad').height();
 
-	return pos;
+        return pos;
     };
 
     $('#trackpad').bind('mousedown', function() {
-	touching = true;
+        touching = true;
     });
 
     $('#trackpad').bind('mouseup', function() {
-	touching = false;
+        touching = false;
     });
 
     $('#trackpad').bind('mousemove', function() {
     });
-    
+
     $('#trackpad')[0].addEventListener('touchstart', function(e) {
-	e.preventDefault();
-	touching = true;
-	socket.emit('touchstart', getPosition(e.touches[0]));
+        e.preventDefault();
+        touching = true;
+        socket.emit('touchstart', getPosition(e.touches[0]));
     }, false);
 
     $('#trackpad')[0].addEventListener('touchend', function(e) {
-	e.preventDefault();
-	touching = false;
-	socket.emit('touchend', getPosition(e.touches[0]));
+        e.preventDefault();
+        touching = false;
+        socket.emit('touchend', getPosition(e.touches[0]));
     }, false);
 
     var then = new Date();
 
     $('#trackpad')[0].addEventListener('touchmove', function(e) {
-	e.preventDefault();
+        e.preventDefault();
 
-	var delay = 1;
+        var delay = 1;
 
-	// Wait at least `delay` milliseconds between each touch movement.
-	var now = new Date();
+        // Wait at least `delay` milliseconds between each touch movement.
+        var now = new Date();
 
-	if (now.getTime() - then.getTime() < delay) {
-	    return;
-	} else {
-	    then = new Date();
-	}
-	
-	var touch = e.touches[0];
-	var pos = getPosition(touch);
+        if (now.getTime() - then.getTime() < delay) {
+            return;
+        } else {
+            then = new Date();
+        }
 
-	if (touching) {
-	    socket.emit('movement', pos);
-	}
+        var touch = e.touches[0];
+        var pos = getPosition(touch);
+
+        if (touching) {
+            socket.emit('movement', pos);
+        }
     }, false);
 
     $('#click').click(function() {
-	socket.emit('click');
+        socket.emit('click');
     });
 });
--- index.js
+++ index.js
@@ -20,76 +20,76 @@ io.on('connection', function(socket) {
     console.log('a user connected');
 
     socket.on('disconnect', function() {
-	console.log('user disconnected');
+        console.log('user disconnected');
     });
 
     let oldpos;
     let anchor;
 
     socket.on('touchstart', function(position) {
-	anchor = position;
-	
-	let pos = robot.getMousePos();
-	oldpos = { x : pos.x / width, y : pos.y / height };
+        anchor = position;
+
+        let pos = robot.getMousePos();
+        oldpos = { x: pos.x / width, y: pos.y / height };
     });
 
     socket.on('movement', function(position) {
-	let cursorpos = { x : position.x, y : position.y };
-	
-	// Transform the position relative to anchor.  I.e. when the user starts
-	// a new press, the previous mouse position is recorded, along with the
-	// point on the screen they press. If the user's touch position is at
-	// the anchor it will be as if their cursor is at the recorded mouse
-	// position. All motion of the mouse is relative to this anchor point.
-	//
-	// c.f. absolute positioning, where the user trackpad has a one-to-one
-	// correspondance with the mouse cursor on the screen at all times,
-	// i.e. if we press the top left corner the mouse will move to the top
-	// left
-	if (anchor !== undefined && oldpos !== undefined) {
-	    position.x = position.x - anchor.x + oldpos.x;
-	    position.y = position.y - anchor.y + oldpos.y;
-	}
+        let cursorpos = { x: position.x, y: position.y };
 
-	// Bounds checking. Move anchor as we go "into" edge of screen.
-	if (position.x < 0) {
-	    // Reset anchor, so that the edge of the screen is the new
-	    // anchor point
-	    anchor.x = cursorpos.x;
-	    oldpos.x = 0;
-	    
-	    position.x = 0;
-	}
-	if (position.y < 0) {
-	    anchor.y = cursorpos.y;
-	    oldpos.y = 0;
+        // Transform the position relative to anchor.  I.e. when the user starts
+        // a new press, the previous mouse position is recorded, along with the
+        // point on the screen they press. If the user's touch position is at
+        // the anchor it will be as if their cursor is at the recorded mouse
+        // position. All motion of the mouse is relative to this anchor point.
+        //
+        // c.f. absolute positioning, where the user trackpad has a one-to-one
+        // correspondance with the mouse cursor on the screen at all times,
+        // i.e. if we press the top left corner the mouse will move to the top
+        // left
+        if (anchor !== undefined && oldpos !== undefined) {
+            position.x = position.x - anchor.x + oldpos.x;
+            position.y = position.y - anchor.y + oldpos.y;
+        }
 
-	    position.y = 0;
-	}
+        // Bounds checking. Move anchor as we go "into" edge of screen.
+        if (position.x < 0) {
+            // Reset anchor, so that the edge of the screen is the new
+            // anchor point
+            anchor.x = cursorpos.x;
+            oldpos.x = 0;
 
-	// Mac OS X doesn't seem to like either coordinate == 1
-	let edge = 0.9999;
-	if (position.x > 1) {
-	    anchor.x = cursorpos.x;
-	    oldpos.x = edge;
-	    
-	    position.x = edge;
-	}
-	if (position.y > 1) {
-	    anchor.y = cursorpos.y;
-	    oldpos.y = edge;
+            position.x = 0;
+        }
+        if (position.y < 0) {
+            anchor.y = cursorpos.y;
+            oldpos.y = 0;
 
-	    position.y = edge;
-	}
-	
-	// Scale normalised position to screen size.
-	robot.moveMouse(position.x * width, position.y * height);
-	console.log(position);
+            position.y = 0;
+        }
+
+        // Mac OS X doesn't seem to like either coordinate == 1
+        let edge = 0.9999;
+        if (position.x > 1) {
+            anchor.x = cursorpos.x;
+            oldpos.x = edge;
+
+            position.x = edge;
+        }
+        if (position.y > 1) {
+            anchor.y = cursorpos.y;
+            oldpos.y = edge;
+
+            position.y = edge;
+        }
+
+        // Scale normalised position to screen size.
+        robot.moveMouse(position.x * width, position.y * height);
+        console.log(position);
     });
 
     socket.on('click', function() {
-	robot.mouseClick();
-	console.log('clicked');
+        robot.mouseClick();
+       	console.log('clicked');
     });
 });
 
